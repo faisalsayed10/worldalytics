f483698a7e56d066b20bb054d4d61580b082e3de
faisalsayed10 <faisal.sayed502@gmail.com>
2021-01-15 15:16:55 +0530

    setting up the layout

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import Layout from '../components/layout/Layout'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return <Layout>main</Layout>;
+}
+
